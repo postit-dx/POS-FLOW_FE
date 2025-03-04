dcadd993187a9e61d4b56b2685dc15591640efa1
YuJeongHyun <0703olivia@naver.com>
2025-03-04 15:45:18 +0900

    refactor: Add TableField

--- src/components/vehicle/Dashboard.js
+++ src/components/vehicle/Dashboard.js
@@ -2,12 +2,21 @@ import React, {useState} from 'react';
 import '../../styles/css/vehicle/Dashboard.css';
 import Modal from 'react-modal';
 import AddForm from './AddForm.js';
-import Vehicle from './Vehicle.js';
+
+import {TableThead, TableTbody} from '../TableField.js';
 
 Modal.setAppElement('#root');
 
 function Dashboard({vehicles, fetchVehicles}) {
     const [modalOpen, setModalOpen] = useState(false);
+
+    const tableHeaders = [
+        "출발일시", "차량번호", "컨테이너번호", "Po No", 
+        "공급사 ITEM", "용차여부", "운송코드", "진행상태", 
+        "완료일시", "등록방법", "출입신청번호", "계량예약번호", 
+        "차량승인여부", "검수대상여부", "승인부서", "승인자", "승인일자"
+    ];
+    
 
     return (
         <div className='table-container'>
@@ -26,43 +35,8 @@ function Dashboard({vehicles, fetchVehicles}) {
 
 
             <table className='vehicle-table'>
-                <thead>
-                    <tr>
-                        <th>출발일시</th>
-                        <th>차량번호</th>
-                        <th>컨테이너번호</th>
-                        <th>Po No</th>
-                        <th>공급사 ITEM</th>
-                        <th>용차여부</th>
-                        <th>운송코드</th>
-                        <th>진행상태</th>
-                        <th>완료일시</th>
-                        <th>등록방법</th>
-                        <th>출입신청번호</th>
-                        <th>계량예약번호</th>
-                        <th>차량승인여부</th>
-                        <th>검수대상여부</th>
-                        <th>승인부서</th>
-                        <th>승인자</th>
-                        <th>승인일자</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        vehicles.length > 0 ?
-                        (vehicles.map((v, index) => (
-                            <tr key={index}>
-                                <Vehicle v={v}/>
-                            </tr>
-                        )))
-                        : 
-                        <tr>
-                            <td colSpan={16} className='empty-vehicle'>
-                                no data
-                            </td>
-                        </tr>
-                    }
-                </tbody>
+                <TableThead tableHeaders={tableHeaders}/>
+                <TableTbody vehicles={vehicles} total={tableHeaders.length}/>
             </table>
         </div>
     );
